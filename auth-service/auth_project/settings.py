f7e6be6bd5359907d4427197407580207edbcdc2
sinan-prvt <mohamedsinan9400@gmail.com>
2026-01-17 11:29:25 +0530

    Updated-settings-in-auth-service

--- auth-service/auth_project/settings.py
+++ auth-service/auth_project/settings.py
@@ -11,7 +11,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-please-change")
 
 
-DEBUG = os.getenv("DEBUG", "False").lower() in ("1", "true", "yes")
+DEBUG = False
+
 
 ALLOWED_HOSTS = [
     "*"
